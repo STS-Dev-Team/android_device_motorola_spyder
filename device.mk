cf3497f028929fc7fdab408abf39931200f84839
Hashcode <hashcode0f@gmail.com>
2013-03-11 22:45:44 -0700

    build changes

--- device.mk
+++ device.mk
@@ -1,11 +1,7 @@
 #
 # This is the product configuration for a full spyder
 #
-ifeq ($(BOARD_USES_KEXEC),true)
 $(call inherit-product, device/motorola/omap4-kexec-common/common.mk)
-else
-$(call inherit-product, device/motorola/omap4-common/common.mk)
-endif
 
 DEVICE_FOLDER := device/motorola/spyder
 
@@ -14,10 +10,10 @@ DEVICE_FOLDER := device/motorola/spyder
 
 # Hardware HALs
 PRODUCT_PACKAGES += \
+    power.spyder \
     camera.spyder \
-    lights.spyder
-
-#    hwcomposer.spyder
+    lights.spyder \
+    hwcomposer.spyder
 
 # Modem
 PRODUCT_PACKAGES += \
@@ -26,13 +22,10 @@ PRODUCT_PACKAGES += \
     libreference-cdma-sms
 
 # Kexec files and ti ducati or rootfs files
-ifeq ($(BOARD_USES_KEXEC),true)
 PRODUCT_COPY_FILES += \
-    $(DEVICE_FOLDER)/prebuilt/etc/firmware/ducati-m3.bin:system/etc/firmware/ducati-m3.bin \
     $(DEVICE_FOLDER)/kexec/devtree:system/etc/kexec/devtree \
     $(OUT)/ramdisk.img:system/etc/kexec/ramdisk.img \
     $(OUT)/kernel:system/etc/kexec/kernel
-endif
 
 # Prebuilts
 PRODUCT_COPY_FILES += \
@@ -41,24 +34,9 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
     $(DEVICE_FOLDER)/prebuilt/etc/vold.fstab:system/etc/vold.fstab
 
-ifneq ($(BOARD_USES_KEXEC),true)
-LOCAL_KERNEL := device/motorola/spyder/kernel
-
-PRODUCT_COPY_FILES += \
-        $(LOCAL_KERNEL):kernel
-
-# copy all kernel modules under the "modules" directory to system/lib/modules
-PRODUCT_COPY_FILES += $(shell \
-    find device/motorola/spyder/modules -name '*.ko' \
-    | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
-    | tr '\n' ' ')
-endif
-
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/motorola/omap4-common/proprietary/vzw/verizon.mk)
 $(call inherit-product-if-exists, vendor/motorola/spyder/spyder-vendor.mk)
-ifneq ($(BOARD_USES_KEXEC),true)
-$(call inherit-product-if-exists, vendor/motorola/spyder/spyder-vendor-pvr.mk)
-$(call inherit-product-if-exists, vendor/motorola/spyder/spyder-vendor-stock-camera.mk)
 $(call inherit-product-if-exists, vendor/motorola/spyder/spyder-vendor-stock-ducati.mk)
-endif
+$(call inherit-product-if-exists, vendor/motorola/omap4-common/common-vendor-pvr.mk)
+
